4e41d51c1132a6ef26c2153eb2a845e0bd010525
Rebecca Ruth <Ruth0506@live.missouristate.edu>
2020-11-12 23:21:38 -0600

    level 1 2

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -4,6 +4,7 @@ var tableData = data;
 // YOUR CODE HERE!
 
 //level 1
+//chart 
 var tbody = d3.select("tbody");
 
 console.log(data);
@@ -17,4 +18,40 @@ data.forEach(function(ufo) {
     cell.text(value);
    });
  });
+
+//button
+var button = d3.select("#filter-btn");
+var form = d3.select("form");
+button.on("click", runEnter);
+form.on("submit", runEnter);
+function runEnter() {
+  d3.event.preventDefault();
+  var inputElement = d3.select(".form-control");
+  var inputValue = inputElement.property("value");
+  console.log(inputElement);
+  console.log(inputValue);
+  var results = tableData.filter(ufo => ufo.datetime === inputValue);
+  tbody.html("");
+  console.log(results)
+  if (results == 0){
+    tbody.text("No UFO Sightings")
+  }
+  else {
+    results.forEach((ufo) => {
+      var row = tbody.append("tr");
+      Object.entries(ufo).forEach(function([key, value]) {
+        console.log(key, value);
+        var cell = row.append("td");
+      cell.text(value);
+      });
+    });
+  };
+};
+
+//level 2
+
+
+
+
+
 
