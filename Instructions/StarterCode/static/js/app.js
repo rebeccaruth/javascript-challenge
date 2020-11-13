a28a0b40cf2dbf8790b8970c0229dd2e2e4cfc45
Rebecca Ruth <Ruth0506@live.missouristate.edu>
2020-11-12 22:31:58 -0600

    level 1

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -2,3 +2,19 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+//level 1
+var tbody = d3.select("tbody");
+
+console.log(data);
+
+data.forEach(function(ufo) {
+  console.log(ufo);
+  var row = tbody.append("tr");
+  Object.entries(ufo).forEach(function([key, value]) {
+     console.log(key, value);
+     var cell = row.append("td");
+    cell.text(value);
+   });
+ });
+
