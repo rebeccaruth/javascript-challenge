a7d7410eb76e3019d035a30491d907b8c9566ee1
Rebecca Ruth <Ruth0506@live.missouristate.edu>
2020-11-13 01:27:06 -0600

    level 2

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -20,6 +20,36 @@ data.forEach(function(ufo) {
  });
 
 //button
+//var button = d3.select("#filter-btn");
+//var form = d3.select("form");
+//button.on("click", runEnter);
+//form.on("submit", runEnter);
+//function runEnter() {
+//  d3.event.preventDefault();
+//  var inputElement = d3.select(".form-control");
+//  var inputValue = inputElement.property("value");
+//  console.log(inputElement);
+//  console.log(inputValue);
+//  var results = tableData.filter(ufo => ufo.datetime === inputValue);
+//  tbody.html("");
+//  console.log(results)
+//  if (results == 0){
+//    tbody.text("No UFO Sightings")
+//  }
+//  else {
+//    results.forEach((ufo) => {
+//      var row = tbody.append("tr");
+//      Object.entries(ufo).forEach(function([key, value]) {
+//        console.log(key, value);
+//        var cell = row.append("td");
+//      cell.text(value);
+//      });
+//    });
+//  };
+//};
+
+//level 2
+
 var button = d3.select("#filter-btn");
 var form = d3.select("form");
 button.on("click", runEnter);
@@ -30,7 +60,7 @@ function runEnter() {
   var inputValue = inputElement.property("value");
   console.log(inputElement);
   console.log(inputValue);
-  var results = tableData.filter(ufo => ufo.datetime === inputValue);
+  var results = tableData.filter(ufo => ufo.datetime == inputValue || ufo.city == inputValue || ufo.state == inputValue ||ufo.country == inputValue|| ufo.shape == inputValue);
   tbody.html("");
   console.log(results)
   if (results == 0){
@@ -46,12 +76,4 @@ function runEnter() {
       });
     });
   };
-};
-
-//level 2
-
-
-
-
-
-
+}
